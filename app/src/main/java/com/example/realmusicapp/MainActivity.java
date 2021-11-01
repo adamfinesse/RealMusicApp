d12d5e8ff13cfb21f829cc1cc370e60840d2ecb7
adam <sirassassinshoes@gmail.com>
2021-11-01 16:16:08 -0400

    fixes

--- app/src/main/java/com/example/realmusicapp/MainActivity.java
+++ app/src/main/java/com/example/realmusicapp/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 1337;
-
     private static final String REDIRECT_URI = "com.example.musicapp";
     private static final String CLIENT_ID = "e30929e731664b3f86b922d87115dc59";
 
---
+++ app/src/main/java/com/example/realmusicapp/ui/login/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.example.realmusicapp.ui.login;
+import androidx.appcompat.app.AppCompatActivity;
+
+//import com.example.realmusicapp.databinding.ActivityLoginBinding;
+public class LoginActivity extends AppCompatActivity{
+
+
+}
