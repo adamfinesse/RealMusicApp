078e7f1023d5928a920d4312c2e1564f49fc6c00
JOTHECABLEGUY <jzdube99@gmail.com>
2021-11-05 12:59:12 -0400

    added playlist help button

---
+++ app/src/main/java/com/example/spotifyplaylist/HelpActivity2.java
@@ -0,0 +1,14 @@
+package com.example.spotifyplaylist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelpActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help2);
+    }
+}
--- app/src/main/java/com/example/spotifyplaylist/PlayViewActivity.java
+++ app/src/main/java/com/example/spotifyplaylist/PlayViewActivity.java
@@ -2,7 +2,10 @@ package com.example.spotifyplaylist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class PlayViewActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class PlayViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_view);
+    }
+    public void helpMenu2(View v){
+        //Button a = (Button)v;
+        Intent i = new Intent(this, HelpActivity2.class);
+        startActivity(i);
+        Log.d("helpmenu2","help menu 2 button clicked");
     }
 }
