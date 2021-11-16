3cde7e6519f4a93078b79ff4ecc920bbee1078a3
pikeda <pikeda@vcu.edu>
2021-11-15 22:54:39 -0500

    Added a test

--- app/src/test/java/com/example/spotifyplaylist/ExampleUnitTest.java
+++ app/src/test/java/com/example/spotifyplaylist/ExampleUnitTest.java
@@ -4,14 +4,26 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.example.spotifyplaylist.*;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    //Does not yet test anything but to help make sure login is working
+    @Test
+    public void loginCheck(){
+        String username = "freeAccount";
+        String password = "password";
+        assertFalse(false);
+    }
+
 }
